418c17b5548ef018019c90562eaaa0e2ed354fb7
cdr <adarshcp2077@gmail.com>
2026-08-27 17:00:57 -0400

    feat: implemented change size funcitonality
    - removed redundant console logs and tables
    - added click event listener for change size button
    - handleClick current works for chaging grid size dynamically.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use-strict";
 
 function generateGrid(dimension = 16) {
-  console.log("inside generateGrid");
   const divList = [];
   for (let i = 0; i < dimension; i++) {
     divList.push(document.createElement("div"));
@@ -9,10 +8,36 @@ function generateGrid(dimension = 16) {
       divList[i].appendChild(document.createElement("div"));
     }
   }
-  console.table(divList);
   grid.replaceChildren(...divList);
+}
+
+function promptForSize() {
+  let dimension = prompt(
+    "Enter a grid length between 1 and 100 for generating a sqare grid.",
+  );
+  while (
+    dimension &&
+    (isNaN(dimension) ||
+      !Number.isInteger(Number(dimension)) ||
+      parseInt(dimension) < 1 ||
+      parseInt(dimension) > 100)
+  ) {
+    dimension = prompt(
+      "Invalid grid length. Enter an integer number between 1 and 100.",
+    );
+  }
+  if (dimension) {
+    generateGrid(parseInt(dimension));
+  }
+}
+
+function handleClick(e) {
+  if (e.target.id === "btn-change-size") {
+    promptForSize();
+  }
 }
 
 const buttons = document.querySelector(".btn-container");
 const grid = buttons.nextElementSibling;
 generateGrid();
+buttons.addEventListener("click", handleClick);
